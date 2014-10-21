6d0f08417dec58876e25aa7030a902575464a95f
David Mandel <MandelDL@gmail.com>
2014-10-21 17:53:00 -0400

    Included JQuery UI link, made sortable

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
     //Function 1 - Put stuff in input, click button, it gets added as div with class item to bottom of the div with class list.
     $('#button').click(function(){
         var toAdd = $('input[name=checkListItem]').val();
-        $('.list').append('<div class="item">'+toAdd+'</div>');
+        $('.list').append('<li class="item">'+toAdd+'</li>');
         });
     // Function 2 - Whenever you click on something with a class of item, remove this.
     $(document).on('click','.item', function(){
         $(this).remove();
         });
+
+    $('.list').sortable();
 });
